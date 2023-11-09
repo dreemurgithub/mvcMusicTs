7c78ffed1d9350526682564b65926fc4b91bd3a2
LeThanhDat <Le Thanh Dat>
2023-11-09 17:32:05 +0700

    cache valid youtube id from 1st check

--- src/validations/youtube.validate.ts
+++ src/validations/youtube.validate.ts
@@ -2,7 +2,7 @@ import { rejects } from "assert";
 import { regex5minLess } from "./regex.validate";
 import * as yt from "youtube-search-without-api-key";
 import { resolve } from "path";
-
+import { ytCacheRepository } from "@/config/database/typeorm";
 export const checkDuration = async (id: string) => {
   const videosIdArr = await yt.search(id);
   if (!regex5minLess.test(videosIdArr[0].duration_raw)) return false;
@@ -10,14 +10,24 @@ export const checkDuration = async (id: string) => {
 };
 
 export const validateYoutubeId = (id: string) => {
-  return new Promise(async(resolve: any, rejects: any) => {
-    const videosIdArr = await yt.search(id);
-    if (videosIdArr[0].id.videoId === id) return resolve( true);
-    return rejects( false)
+  return new Promise(async (resolve: any, rejects: any) => {
+    const cacheCheck = await ytCacheRepository.findOne({
+      where: { youtubeId: id },
+    });
+    if (cacheCheck) {
+      return resolve(true);
+    } else {
+      const videosIdArr = await yt.search(id);
+      if (videosIdArr[0].id.videoId === id) {
+        await ytCacheRepository.save({ youtubeId: id });
+        return resolve(true);
+      }
+      return rejects(false);
+    }
   });
 };
 
-export const validateYoutubeSongList = (songList: string [])=>{
-  const checkYoutubeList = songList.map(id => validateYoutubeId(id))
-  return Promise.allSettled(checkYoutubeList)
-}
+export const validateYoutubeSongList = (songList: string[]) => {
+  const checkYoutubeList = songList.map((id) => validateYoutubeId(id));
+  return Promise.allSettled(checkYoutubeList);
+};
