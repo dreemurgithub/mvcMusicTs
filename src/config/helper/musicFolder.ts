f6e2fb38f190e9ecd1db31817ab8fef8172f5d73
LeThanhDat <Le Thanh Dat>
2023-11-09 10:16:07 +0700

    download music and check for youtubeid with one or list id

--- src/config/helper/musicFolder.ts
+++ src/config/helper/musicFolder.ts
@@ -2,7 +2,6 @@ import path from "path";
 import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
-const downloadKey = process.env.DOWNLOAD_MUSIC;
 
 export const checkPath = path.join(__dirname, `../../../data/_k3y2tVWK74`);
 export const allMusicId = () => {
@@ -18,18 +17,9 @@ export const idToMusic = (id: String) => {
     return {songPath: path.join(file, fileList[0]), success: true};
   } catch {
     return {message:"Download the song first", success: false, songPath:''}
+    // error check to prevent ts bug
   }
 };
-export const options = (id: string) => {
-  return {
-    method: "GET",
-    url: "https://youtube-mp36.p.rapidapi.com/dl",
-    params: { id },
-    headers: {
-      "X-RapidAPI-Key": downloadKey,
-      "X-RapidAPI-Host": "youtube-mp36.p.rapidapi.com",
-    },
-  };
-};
+
 
 export const dataFolder = path.join(__dirname, "../../../data");
